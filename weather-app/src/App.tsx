87cc34b77184a749df410b19364f49c65f21a470
jeanmarie farina <farina.jeanmarie@outlook.com>
2022-04-13 11:17:50 +0200

    added chart

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,14 +1,62 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import Search from './Search.tsx'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+interface DataWeather{
+  list: {
+    dt: number
+      main: {
+        temp: number
+        feels_like: number
+        temp_min: number
+        temp_max: number
+        pressure: number
+        sea_level: number
+        grnd_level: number
+        humidity: number
+        temp_kf: number
+      }
+  }[]
+
+}
 
 function App() {
-  const [weather,setWeather] = useState({})
+  const [weather,setWeather] = useState<DataWeather>()
+  const dataTemp: Highcharts.SeriesOptionsType[] = []
+  const [options,setOptions ]= useState<Highcharts.Options>({
+    title: {
+        text: 'Weather for the 5 next days'
+    },
+    xAxis: {
+      type: 'datetime'
+    },
+    yAxis: {
+      title: {
+          text: 'temperature'
+      }
+    },
+    series: [{type:'line',data:[]}]
+  })
+  useEffect(()=>{
+    if(weather !== undefined){
+      weather.list.forEach(element => {
+        dataTemp.push([element.dt*1000,element.main.temp])
+        
+        setOptions((x):Highcharts.Options=>({...x,series:{data : dataTemp}}))
+        console.log(options)
+      })
+    }
+  },[weather])
   return (
     <div className="App">
       <h1>Weather app</h1>
       <Search setWeather={setWeather}/>
-      <div>{JSON.stringify(weather)}</div>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+      />
     </div>
   );
 }
